0d9344b51572d4ef7ea28a4b4b5c97f8f8de6ec7
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-03-05 18:39:27 +0300

    qasdcfghj

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,9 +35,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "678a3645245d3d76998f37e1b8373b55
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b43edb74756e10338d22340b8ffb3017",
-"/": "b43edb74756e10338d22340b8ffb3017",
-"main.dart.js": "cf10588661a5a27a46150ac538155c6e",
+"index.html": "ed195e21c60a519bc8de281a1f632f83",
+"/": "ed195e21c60a519bc8de281a1f632f83",
+"main.dart.js": "58261614156a75c12ab0cd9054c198c6",
 "manifest.json": "faf4d729ef68108b7e73a8069c511994",
 "version.json": "a19ab55720d503400ef45eb9b369a8d1"};
 // The application shell files that are downloaded before a service worker can
--- lib/features/market/ui/market_screen.dart
+++ lib/features/market/ui/market_screen.dart
@@ -13,6 +13,8 @@ class MarketScreen extends StatefulWidget {
 }
 
 class _MarketScreenState extends State<MarketScreen> {
+  final controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -24,61 +26,69 @@ class _MarketScreenState extends State<MarketScreen> {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            child: RepaintBoundary(
+          body: Scrollbar(
+            showTrackOnHover: false,
+            controller: controller,
+            interactive: true,
+            thickness: 10,
+            child: SingleChildScrollView(
+              controller: controller,
               child: SizedBox(
                 width: size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
+                child: RepaintBoundary(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Row(
+                          children: [
+                            InkWell(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Icon(
+                                  Icons.arrow_back_ios_new,
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.1),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          InkWell(
-                              onTap: () {
-                                Navigator.pop(context);
-                              },
-                              child: Icon(
-                                Icons.arrow_back_ios_new,
-                                color: Colors.white,
-                              )),
+                          SvgPicture.asset(
+                            "assets/icons/bag.svg",
+                            width: 100,
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "SHOP",
+                            style:
+                                AppFonts.font29w400.copyWith(color: Colors.white),
+                          )
                         ],
                       ),
-                    ),
-                    SizedBox(height: size.height * 0.1),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          "assets/icons/bag.svg",
-                          width: 100,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          "Skins",
+                          style: AppFonts.font20w400.copyWith(),
                         ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          "SHOP",
-                          style: AppFonts.font29w400
-                              .copyWith(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        "Skins",
-                        style: AppFonts.font20w400.copyWith(),
                       ),
-                    ),
-                    SizedBox(height: 40),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [1, 2, 3, 2, 3, 2, 3, 2, 3]
-                            .map((e) => MarketPromoCard())
-                            .toList()),
-                  ],
+                      SizedBox(height: 40),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [1, 2, 3, 2, 3, 2, 3, 2, 3]
+                              .map((e) =>
+                                  RepaintBoundary(child: MarketPromoCard()))
+                              .toList()),
+                    ],
+                  ),
                 ),
               ),
             ),
