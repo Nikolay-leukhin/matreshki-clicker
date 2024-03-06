f1c3cf273b0134d16770c3f42d89db8aaec32fdd
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-03-06 18:44:31 +0300

    pipka

--- lib/features/market/ui/market_screen.dart
+++ lib/features/market/ui/market_screen.dart
@@ -5,6 +5,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:matreshka/features/market/data/market_repository.dart';
 import 'package:matreshka/features/market/logic/cubit/market_cubit.dart';
+import 'package:matreshka/features/market/ui/market_doll_card.dart';
+import 'package:matreshka/features/market/ui/market_promo_card.dart';
 import 'package:matreshka/models/market_model.dart';
 import 'package:matreshka/utils/colors.dart';
 import 'package:matreshka/utils/fonts.dart';
@@ -65,7 +67,7 @@ class _MarketScreenState extends State<MarketScreen> {
                     children: [
                       SvgPicture.asset(
                         "assets/icons/bag.svg",
-                        width: 100,
+                        width: 60,
                       ),
                       const SizedBox(
                         height: 8,
@@ -114,7 +116,7 @@ class _MarketScreenState extends State<MarketScreen> {
                     ),
                   ),
                   const SizedBox(
-                    height: 20,
+                    height: 5,
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -130,7 +132,7 @@ class _MarketScreenState extends State<MarketScreen> {
                   ),
                   const SizedBox(height: 10),
                   SizedBox(
-                    height: size.height * 0.5,
+                    height: size.width * 0.85 + 10,
                     child: BlocBuilder<MarketCubit, MarketState>(
                       builder: (context, state) {
                         if (state is MarketLoading) {
@@ -150,197 +152,6 @@ class _MarketScreenState extends State<MarketScreen> {
                         }
                         return Text("fail to load");
                       },
-                    ),
-                  ),
-                ],
-              )),
-        ),
-      ),
-    );
-  }
-}
-
-class MarketPromoCard extends StatelessWidget {
-  final MarketPromoModel promo;
-
-  const MarketPromoCard({super.key, required this.promo});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-
-    return Padding(
-      padding: const EdgeInsets.only(left: 10),
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        height: 93,
-        width: size.width * 0.85,
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-            color: Colors.transparent, borderRadius: BorderRadius.circular(16)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-              height: 93,
-              width: size.width - 20,
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              alignment: Alignment.center,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  color: AppColors.cD9D9D9.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(16)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 67,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.white.withOpacity(0.4)),
-                    padding: const EdgeInsets.all(10),
-                    child: const Icon(
-                      Icons.free_breakfast_outlined,
-                      size: 40,
-                      color: AppColors.c322A2A,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  SizedBox(
-                    width: size.width * 0.46,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(promo.name,
-                            overflow: TextOverflow.ellipsis,
-                            style: AppFonts.font20w400.copyWith(
-                                color: AppColors.c322A2A,
-                                fontWeight: FontWeight.bold)),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/images/coin.png',
-                              width: 27,
-                              fit: BoxFit.fitWidth,
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              promo.price.toString(),
-                              overflow: TextOverflow.ellipsis,
-                              style: AppFonts.font15w400
-                                  .copyWith(color: AppColors.c322A2A),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )),
-        ),
-      ),
-    );
-  }
-}
-
-class MarketMatreshkaCard extends StatelessWidget {
-  final MarketSkinModel skin;
-
-  const MarketMatreshkaCard({super.key, required this.skin});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-
-    return Padding(
-      padding: const EdgeInsets.only(left: 10),
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        height: size.width * 0.55 + 85,
-        width: size.width * 0.55 + 85,
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-            color: Colors.transparent, borderRadius: BorderRadius.circular(16)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-              height: size.width * 0.85,
-              width: size.width * 0.85,
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              alignment: Alignment.center,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  color: AppColors.cD9D9D9.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(16)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      SizedBox(
-                        width: size.width * 0.46,
-                        child: Image.asset(
-                          skin.iconPath,
-                          height: size.width * 0.55,
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            skin.coinIconPath,
-                            height: 50,
-                            fit: BoxFit.fitHeight,
-                          ),
-                          Text(skin.coinName,
-                              style: AppFonts.font20w400
-                                  .copyWith(color: AppColors.c322A2A))
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const Divider(
-                    height: 2,
-                  ),
-                  const Spacer(),
-                  Container(
-                    height: 52,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.white.withOpacity(0.4)),
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/images/coin.png',
-                          height: 30,
-                          fit: BoxFit.fitHeight,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          skin.price.toString(),
-                          style: AppFonts.font18w400
-                              .copyWith(color: AppColors.c322A2A),
-                        )
-                      ],
                     ),
                   ),
                 ],
