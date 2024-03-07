72a55955711695f246a63fce43171db11338909e
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-03-07 18:14:30 +0300

    dfghk

--- lib/features/boosts/data/boosts_repository.dart
+++ lib/features/boosts/data/boosts_repository.dart
@@ -1,10 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:matreshka/features/main/data/main_repository.dart';
+import 'package:matreshka/models/boost_model.dart';
 import 'package:matreshka/services/firebase/firebase_collections.dart';
 
 class BoostsRepository {
-  // Future<void> loadBoosts() async {
-  //   final db = FirebaseCollections.marketCollection.get();
-    
-  // }
+  final MainRepository mainRepository;
 
-  Future<void> buyBoost() async {}
+  BoostsRepository({required this.mainRepository});
+
+  Future<void> rechargeEnergy() async {}
+
+  Future<void> incrementScorePerClick(BoosterModel boost) async {
+    final userDoc =  FirebaseCollections.userCollection
+        .doc(mainRepository.user.id.toString());
+    await userDoc.update({'score_per_click': FieldValue.increment(5)});
+
+    await mainRepository.payForItem(boost.price);
+  }
+
+  Future<void> extendEnergyLimit(BoosterModel boost) async {
+    final userDoc =  FirebaseCollections.userCollection
+        .doc(mainRepository.user.id.toString());
+    await userDoc.update({'max_energy': FieldValue.increment(5)});
+
+    await mainRepository.payForItem(boost.price);
+  }
 }
